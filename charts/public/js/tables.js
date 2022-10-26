c6ebd2074ba52b2469b7aafa51f7ed3bf3c16985
Mikhail Iskrytski <miskrytsi@solvd.com>
2022-10-26 17:45:34 +0300

    Fix table 0

--- charts/public/js/tables.js
+++ charts/public/js/tables.js
@@ -1,9 +1,43 @@
 $(document).ready(function () {
     $('#table_0').DataTable(
         {
+            paging: false,
+            fixedHeader: true,
             columnDefs: [
                 {
                     target: 0,
+                    visible: false,
+                },
+                {
+                    target: 3,
+                    visible: false,
+                },
+                {
+                    target: 4,
+                    visible: false,
+                },
+                {
+                    target: 5,
+                    visible: false,
+                },
+                {
+                    target: 6,
+                    visible: false,
+                },
+                {
+                    target: 7,
+                    visible: false,
+                },
+                {
+                    target: 9,
+                    visible: false,
+                },
+                {
+                    target: 10,
+                    visible: false,
+                },
+                {
+                    target:11,
                     visible: false,
                 },
             ],
